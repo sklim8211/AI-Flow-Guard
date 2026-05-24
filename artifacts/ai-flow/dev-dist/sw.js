64b7d28be8a375911bb608245b1f0f07fed76d6b
sklim8211 <55735431-sklim8211@users.noreply.replit.com>
2026-05-24 08:38:53 +0000

    Restored to '332e7fb86fb1b5ce0f8958f3b4878d3726b6f151'
    Replit-Restored-To: 332e7fb86fb1b5ce0f8958f3b4878d3726b6f151

--- artifacts/ai-flow/dev-dist/sw.js
+++ artifacts/ai-flow/dev-dist/sw.js
@@ -81,7 +81,7 @@ define(['./workbox-7e5eb42b'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.a3ae06r9014"
+    "revision": "0.ij32eilet4s"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
