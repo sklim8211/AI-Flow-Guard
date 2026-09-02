c1bfc76bf4c74838a26657387b14502ff555925c
sklim8211 <sklim8211@gmail.com>
2026-09-02 08:13:18 +0000

    Update service worker

--- artifacts/ai-flow/dev-dist/sw.js
+++ artifacts/ai-flow/dev-dist/sw.js
@@ -81,7 +81,7 @@ define(['./workbox-7e5eb42b'], (function (workbox) { 'use strict';
     "revision": "f935fee1215d47eef7d227ef09bed8cd"
   }, {
     "url": "index.html",
-    "revision": "0.0feu4sdq0rk"
+    "revision": "0.kkn7vhvb30s"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
